9740e8082ac525cacaf616b623ad866e063d8c71
cidarosa <lucita.rosa@gmail.com>
2024-06-27 22:24:38 -0300

    Project created, EmailDTO

---
+++ src/test/java/com/github/lucitacastello/integrations/IntegrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucitacastello.integrations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
